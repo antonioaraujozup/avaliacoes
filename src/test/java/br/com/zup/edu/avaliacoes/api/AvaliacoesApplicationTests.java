72f182719b1f9ce1875d716e92e33b17c5c98b7d
Antonio Eloy <101589161+antonioaraujozup@users.noreply.github.com>
2022-04-01 08:42:22 -0300

    Commit inicial.

---
+++ src/test/java/br/com/zup/edu/avaliacoes/api/AvaliacoesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.edu.avaliacoes.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvaliacoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
